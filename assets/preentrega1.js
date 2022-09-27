4d8821077f4d4156719df02672f4fd63416c2a21
Julieta Gordillo <julietagordillo13@hotmail.com>
2022-09-27 14:27:20 -0300

    se completa ciclo

--- assets/preentrega1.js
+++ assets/preentrega1.js
@@ -1,32 +1,47 @@
 // Convertidor de °C a °K
 
-let gradoscelsius = Number(prompt("Introduce grados celsius", 0))
+let gradoscelsius = Number(prompt("Introduce grados celsius, el valor a ingresar tiene que ser mayor que -273.15", 0))
+let mensaje;
+const ceroabs = -273.15
 
+function verificar(numero) {
 
-function verificar(gradoscelsius) {
+    gradoscelsius = numero
 
     if (!isNaN(gradoscelsius)) {
-        gradoskelvin (gradoscelsius)
+        if (gradoscelsius >=
+            ceroabs) {
+            mensaje = alert("El numero está dentro del rango");
+        } else {
+            while (gradoscelsius < ceroabs) {
+                mensaje = alert("El numero está fuera de rango, el valor ingresado tiene que ser mayor que -273.15")
+                gradoscelsius = Number(prompt("Introduce grados celsius, el valor a ingresar tiene que ser mayor que -273.15", 0))
+            }
+        }
 
     } else {
         mensaje = alert("Debes ingresar sólo números")
-        gradoscelsius = Number(prompt("Introduce grados celsius", 0))
-        verificar (gradoscelsius)
+        gradoscelsius = Number(prompt("Introduce grados celsius , el valor a ingresar tiene que ser menor que -273.15", 0))
+        verificar(gradoscelsius)
     }
 }
-
-
-function gradoskelvin(gradoscelsius) {
-    console.log("grados kelvin " + Number(gradoscelsius + 273.15))
-}
-
-
 verificar(gradoscelsius)
 
-if (gradoscelsius < -273.15) {
-    alert("el valor es menor al cero absoluto");
-} else if (gradoscelsius > 100) {
-    alert("el agua está en ebullición")
-} else {
-    alert("el agua está líquida")
+const kelvin = 273.15;
+let gradoskelvin = gradoscelsius + kelvin
+console.log("Grados Kelvin " + Number(gradoskelvin))
+
+switch (true) {
+    case gradoskelvin == 0:
+        console.log("Llegaste al cero absoluto");
+        break;
+    case gradoskelvin > 1 && gradoskelvin < 273:
+        console.log("El agua está congelada");
+        break;
+    case gradoskelvin > 273 && gradoskelvin < 373:
+        console.log("El agua está líquida");
+        break;
+    default:
+        console.log("El agua está en ebullición");
+
 }
